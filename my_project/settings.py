25ed837860ee4e0a331d333cd4a6ac368cdccee4
[lucadm] <[lucadimaio@gmail.com]>
2026-02-16 17:47:50 +0000

    Add heroku as allowed host

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y*v5wfqe)i#628s9d@c((#h5jj$*5!0xb3epwou6$0@lv+m(mz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
